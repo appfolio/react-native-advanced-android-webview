ed46b7eb9b962c6642a635177c73eb62d5bb8caf
Daniel Vicory <daniel.vicory@appfolio.com>
2018-06-20 17:57:27 -0700

    Example app that goes to HTML5 file upload test

--- example/App.js
+++ example/App.js
@@ -8,51 +8,24 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import AndroidWebView from 'react-native-advanced-android-webview';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <AndroidWebView
+        style={styles.webview}
+        source={{ uri: 'https://mobilehtml5.org/ts/?id=23' }}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  webview: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
